5acdaeb5208d93f0ffd974ae76757c273791e095
sagardhiman18 <dhimansagar007@gmail.com>
2020-05-11 20:13:44 +0530

    Instagram Clone

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class ProfileController extends Controller
+{
+    public function index(User $user)
+    {
+        // to check wheather user follows another or not
+        $follows = (Auth()->user()) ? Auth()->user()->following->contains($user->id) : false; 
+        // $user = User::findorFail($user);
+        return view('profiles.index', compact('user', 'follows'));
+    }
+
+    public function edit(\App\User $user)
+    {
+        $this->authorize('update', $user->profile);
+    
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $this->authorize('update', $user->profile);
+
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
+        ]);
+
+        if (request('image')) {
+            $imagePath = request('image')->store('profile', 'public');
+
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save();
+
+            $imageArray = ['image' => $imagePath];
+        }
+
+        Auth()->user()->profile()->update(array_merge(
+            $data,
+            $imageArray ?? []
+        ));
+
+        return redirect('/profile/'.$user->id);
+    }
+
+}
